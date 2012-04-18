7904ebd401ff3eacf129b6581ae2cfb11f147db0
Mike Roda <mike@mikeroda.com>
2012-04-18 07:59:21 -0400

    Added the other Terremark CentOS and RHEL images

--- tmrk.sql
+++ tmrk.sql
@@ -74,6 +74,99 @@ VALUES
 'x86', NULL , NULL , '0'
 );
 
+INSERT INTO `vcl`.`image`
+(`id` ,
+`name` ,
+`prettyname` ,
+`ownerid` ,
+`platformid` ,
+`OSid` ,
+`imagemetaid` ,
+`minram` ,
+`minprocnumber` ,
+`minprocspeed` ,
+`minnetwork` ,
+`maxconcurrent` ,
+`reloadtime` ,
+`deleted` ,
+`test` ,
+`lastupdate` ,
+`forcheckout` ,
+`maxinitialtime` ,
+`project` ,
+`size` ,
+`architecture` ,
+`description` ,
+`usage` ,
+`basedoffrevisionid`)
+VALUES
+('9' , 'tmrk-centos5-64bit', 'CentOS 5 (64-bit)', '1', '1', '39', NULL,
+'1024', '1', '2000', '100', NULL , '5', '0', '0', NOW(), '1', '0', 'vcl', '1500',
+'x86', NULL , NULL , '0'
+);
+
+INSERT INTO `vcl`.`image`
+(`id` ,
+`name` ,
+`prettyname` ,
+`ownerid` ,
+`platformid` ,
+`OSid` ,
+`imagemetaid` ,
+`minram` ,
+`minprocnumber` ,
+`minprocspeed` ,
+`minnetwork` ,
+`maxconcurrent` ,
+`reloadtime` ,
+`deleted` ,
+`test` ,
+`lastupdate` ,
+`forcheckout` ,
+`maxinitialtime` ,
+`project` ,
+`size` ,
+`architecture` ,
+`description` ,
+`usage` ,
+`basedoffrevisionid`)
+VALUES
+('10' , 'tmrk-rhel5-32bit', 'RHEL 5 (32-bit)', '1', '1', '39', NULL,
+'512', '1', '1000', '100', NULL , '5', '0', '0', NOW(), '1', '0', 'vcl', '1500',
+'x86', NULL , NULL , '0'
+);
+
+INSERT INTO `vcl`.`image`
+(`id` ,
+`name` ,
+`prettyname` ,
+`ownerid` ,
+`platformid` ,
+`OSid` ,
+`imagemetaid` ,
+`minram` ,
+`minprocnumber` ,
+`minprocspeed` ,
+`minnetwork` ,
+`maxconcurrent` ,
+`reloadtime` ,
+`deleted` ,
+`test` ,
+`lastupdate` ,
+`forcheckout` ,
+`maxinitialtime` ,
+`project` ,
+`size` ,
+`architecture` ,
+`description` ,
+`usage` ,
+`basedoffrevisionid`)
+VALUES
+('11' , 'tmrk-rhel5-64bit', 'RHEL 5 (64-bit)', '1', '1', '39', NULL,
+'1024', '1', '2000', '100', NULL , '5', '0', '0', NOW(), '1', '0', 'vcl', '1500',
+'x86', NULL , NULL , '0'
+);
+
 INSERT INTO `vcl`.`imagerevision` (
 `id` ,
 `imageid` ,
@@ -90,6 +183,54 @@ VALUES (
 NULL , '8', '1', '1', NOW(), '0', NULL , '1', NULL , 'tmrk-centos5-32bit'
 );
 
+INSERT INTO `vcl`.`imagerevision` (
+`id` ,
+`imageid` ,
+`revision` ,
+`userid` ,
+`datecreated` ,
+`deleted` ,
+`datedeleted` ,
+`production` ,
+`comments` ,
+`imagename`
+)
+VALUES (
+NULL , '9', '1', '1', NOW(), '0', NULL , '1', NULL , 'tmrk-centos5-64bit'
+);
+
+INSERT INTO `vcl`.`imagerevision` (
+`id` ,
+`imageid` ,
+`revision` ,
+`userid` ,
+`datecreated` ,
+`deleted` ,
+`datedeleted` ,
+`production` ,
+`comments` ,
+`imagename`
+)
+VALUES (
+NULL , '10', '1', '1', NOW(), '0', NULL , '1', NULL , 'tmrk-rhel5-32bit'
+);
+
+INSERT INTO `vcl`.`imagerevision` (
+`id` ,
+`imageid` ,
+`revision` ,
+`userid` ,
+`datecreated` ,
+`deleted` ,
+`datedeleted` ,
+`production` ,
+`comments` ,
+`imagename`
+)
+VALUES (
+NULL , '11', '1', '1', NOW(), '0', NULL , '1', NULL , 'tmrk-rhel5-64bit'
+);
+
 INSERT INTO `vcl`.`resource` (
 `id` ,
 `resourcetypeid` ,
@@ -97,4 +238,31 @@ INSERT INTO `vcl`.`resource` (
 )
 VALUES (
 NULL , '13', '8'
+);
+
+INSERT INTO `vcl`.`resource` (
+`id` ,
+`resourcetypeid` ,
+`subid`
+)
+VALUES (
+NULL , '13', '9'
+);
+
+INSERT INTO `vcl`.`resource` (
+`id` ,
+`resourcetypeid` ,
+`subid`
+)
+VALUES (
+NULL , '13', '10'
+);
+
+INSERT INTO `vcl`.`resource` (
+`id` ,
+`resourcetypeid` ,
+`subid`
+)
+VALUES (
+NULL , '13', '11'
 );
